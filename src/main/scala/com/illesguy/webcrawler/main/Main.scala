13c894d372ddf66cdcb34b503a4f017bcecf5daa
Guy Tamas Illes <illesguy@gmail.com>
2019-09-23 08:33:51 +0100

    Added logging.

--- src/main/scala/com/illesguy/webcrawler/main/Main.scala
+++ src/main/scala/com/illesguy/webcrawler/main/Main.scala
@@ -5,27 +5,39 @@ import java.util.concurrent.Executors
 import com.illesguy.webcrawler.crawler.Crawler
 import com.illesguy.webcrawler.errorhandler.IgnoringErrorHandler
 import com.illesguy.webcrawler.parser.{JsoupDocumentRetriever, SubDomainUrlParser}
+import org.slf4j.LoggerFactory
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 object Main extends App {
+  val logger = LoggerFactory.getLogger(getClass.getCanonicalName)
+
   val urlToCrawl = args.toSeq.headOption.getOrElse("https://monzo.com")
 
   val errorHandler = IgnoringErrorHandler
   val parser = new SubDomainUrlParser(JsoupDocumentRetriever, errorHandler)
   val retriesOnTimeout = 3
-  val threadCount = 4
+  val threadCount = 6
   val execCtx = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(threadCount))
 
   val crawler = new Crawler(parser, retriesOnTimeout, execCtx)
 
+  logger.info(s"Starting crawling from $urlToCrawl with $retriesOnTimeout retries on timeout on $threadCount threads")
+
   val startTime = System.currentTimeMillis()
-  val urls = Await.result(crawler.crawlUrl(urlToCrawl), Duration.Inf)
-  val endTime = System.currentTimeMillis()
 
-  urls.foreach(println)
+  crawler.crawlUrl(urlToCrawl).onComplete {
+    case Success(urls) =>
+      val endTime = System.currentTimeMillis()
 
-  val executionTime = endTime - startTime
-  println(s"Found ${urls.size} urls in $executionTime ms.")
+      urls.foreach(println)
+
+      val executionTime = endTime - startTime
+      logger.info(s"Found ${urls.size} urls in $executionTime ms.")
+
+    case Failure(ex) =>
+      logger.error("Exception occurred while crawling through the pages ", ex)
+
+  }(execCtx)
 }
--- src/main/scala/com/illesguy/webcrawler/parser/SubDomainUrlParser.scala
+++ src/main/scala/com/illesguy/webcrawler/parser/SubDomainUrlParser.scala
@@ -3,6 +3,7 @@ package com.illesguy.webcrawler.parser
 import java.net.SocketTimeoutException
 
 import com.illesguy.webcrawler.errorhandler.ErrorHandler
+import org.slf4j.LoggerFactory
 
 import scala.jdk.CollectionConverters._
 import scala.util.Try
@@ -10,15 +11,22 @@ import scala.util.Try
 
 class SubDomainUrlParser(documentRetriever: JsoupDocumentRetriever, errorHandler: ErrorHandler) extends UrlParser {
 
+  private val logger = LoggerFactory.getLogger(getClass.getCanonicalName)
+
   override def getUrls(url: String, retryCount: Int): Try[Seq[String]] = documentRetriever.getDocumentFromUrl(url).map { doc =>
+    logger.debug(f"Processed $url")
     lazy val elements = doc.select("a[href]").asScala.toSeq
     lazy val urls = elements.map(e => getCanonicalUrl(e.attr("abs:href")))
     lazy val subDomainUrls = urls.filter(u => isSubDomain(url, u))
     subDomainUrls.distinct
   }.recover {
-    case ex => errorHandler.handleError(ex, url)
+    case ex =>
+      logger.warn(s"Error occurred, while processing $url, invoking error handler")
+      errorHandler.handleError(ex, url)
   }.recoverWith {
-    case _: SocketTimeoutException if retryCount != 0 => getUrls(url, retryCount - 1)
+    case _: SocketTimeoutException if retryCount != 0 =>
+      logger.warn(f"Call to $url timed out, retrying")
+      getUrls(url, retryCount - 1)
   }
 
   private def getCanonicalUrl(url: String): String = {
